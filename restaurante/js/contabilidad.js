638bf1e53494b54fa7627872691771c9f096b5eb
Simon Rojas <simonrojasc12@gmail.com>
2026-06-11 15:49:41 -0500

    validate_module_contabilidad: validatedata

--- restaurante/js/contabilidad.js
+++ restaurante/js/contabilidad.js
@@ -8,31 +8,23 @@ let tipo_movimiento = document.getElementById("usuario").value;
 function validarFormulario() {
     if (ventas === "" || fecha === "" || ingresos === "" || concepto === "" || valor === "") {
         alert("Por favor, complete todos los campos del formulario.");
-        return false;
     }
     if (!/^\d+$/.test(ventas)) {
         alert("Las ventas solo deben contener números.");
-        return false;
     }
     if (isNaN(Date.parse(fecha))) {
         alert("Por favor, ingrese una fecha válida.");
-        return false;
     }
     if (new Date(fecha) > new Date()) {
         alert("La fecha no puede ser en el futuro.");
-        return false;
     }
     if (!/^\d+(\.\d{1,2})?$/.test(ingresos)) {
         alert("Los ingresos solo deben contener números y pueden tener hasta dos decimales.");
-        return false;
     }
     if (!/^\d+(\.\d{1,2})?$/.test(valor)) {
         alert("El valor solo debe contener números y puede tener hasta dos decimales.");
-        return false;
     }
         if (!/^[a-zA-Z]+$/.test(nombre)) {
         alert("El nombre solo debe contener letras.");
-        return false;
     }
-    return true;
 }
