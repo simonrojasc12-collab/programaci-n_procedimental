d1424373f3ea1cc6daa02fac76f4c30eee504b7e
Simon Rojas <simonrojasc12@gmail.com>
2026-07-16 17:40:46 -0500

    validate_module_contabilidad: Validate

--- restaurante/js/contabilidad.js
+++ restaurante/js/contabilidad.js
@@ -1,4 +1,5 @@
 function validarFormulario() {
+
 let ventas = document.getElementById("ventas").value;
 let fecha = document.getElementById("fecha").value;
 let ingresos = document.getElementById("ingresos").value;
@@ -7,60 +8,65 @@ let valor = document.getElementById("valor").value;
 let tipo_movimiento = document.getElementById("usuario").value;
 
     if (ventas === "" || fecha === "" || ingresos === "" || concepto === "" || valor === "") {
-        console.log("Por favor, complete todos los campos del formulario.");
         Swal.fire({
-            title: "Drag me!",
-            icon: "success",
-            draggable: true
+            title: "Los campos estan vacios",
+            icon: "Error",
         });
+        console.log("Por favor, complete todos los campos del formulario.");
+        return;
     }
     if (!/^\d+$/.test(ventas)) {
-        console.log("Las ventas solo deben contener números.");
         Swal.fire({
-            title: "Drag me!",
-            icon: "success",
-            draggable: true
+            title: "Complete el campo correctamente!",
+            icon: "Error",
         });
+        console.log("Las ventas solo deben contener números.");
+        return;
     }
     if (isNaN(Date.parse(fecha))) {
-        console.log("Por favor, ingrese una fecha válida.");
         Swal.fire({
             title: "Drag me!",
-            icon: "success",
+            icon: "Error",
             draggable: true
         });
+        console.log("Por favor, ingrese una fecha válida.");
+        return;       
     }
     if (new Date(fecha) > new Date()) {
-        console.log("La fecha no puede ser en el futuro.");
         Swal.fire({
             title: "Drag me!",
-            icon: "success",
+            icon: "Error",
             draggable: true
         });
+        console.log("La fecha no puede ser en el futuro.");
+        return;
     }
     if (!/^\d+(\.\d{1,2})?$/.test(ingresos)) {
-        console.log("Los ingresos solo deben contener números y pueden tener hasta dos decimales.");
         Swal.fire({
             title: "Drag me!",
-            icon: "success",
+            icon: "Error",
             draggable: true
         });
+        console.log("Los ingresos solo deben contener números y pueden tener hasta dos decimales.");
+        return;
     }
     if (!/^\d+(\.\d{1,2})?$/.test(valor)) {
-        console.log("El valor solo debe contener números y puede tener hasta dos decimales.");
         Swal.fire({
             title: "Drag me!",
-            icon: "success",
+            icon: "Error",
             draggable: true
         });
+        console.log("El valor solo debe contener números y puede tener hasta dos decimales.");
+        return;
     }
     if (!/^[a-zA-Z]+$/.test(nombre)) {
-        console.log("El nombre solo debe contener letras.");
         Swal.fire({
             title: "Drag me!",
-            icon: "success",
+            icon: "Error",
             draggable: true
         });
+        console.log("El nombre solo debe contener letras.");
+        return;
     }
 }
 
